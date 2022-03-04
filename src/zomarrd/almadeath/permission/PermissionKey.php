067b334230b863ca86bf80da50555afecdc1bfc2
zOmArRD <omarsena25@gmail.com>
2022-03-04 19:39:25 -0400

    Add permissions keys

---
+++ src/zomarrd/almadeath/permission/PermissionKey.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright © 2022 zOmArRD (omar@ghostlymc.live) - All Rights Reserved.
+ *
+ * This is an auto generated file, don't try to modify this by hand lol.
+ *
+ *  $$$$$$\  $$\                         $$$$$$$\                       $$\     $$\       
+ * $$  __$$\ $$ |                        $$  __$$\                      $$ |    $$ |      
+ * $$ /  $$ |$$ |$$$$$$\$$$\   $$$$$$\  $$ |  $$ | $$$$$$\   $$$$$$\ $$$$$$\   $$$$$$$\  
+ * $$$$$$$$ |$$ |$$  _$$  _$$\  \____$$\ $$ |  $$ |$$  __$$\  \____\\_$$  _|  $$  __$$\ 
+ * $$  __$$ |$$ |$$ / $$ / $$ | $$$$$$$ |$$ |  $$ |$$$$$$$$ | $$$$$$$ | $$ |    $$ |  $$ |
+ * $$ |  $$ |$$ |$$ | $$ | $$ |$$  __$$ |$$ |  $$ |$$   ____|$$  __$$ | $$ |$$\ $$ |  $$ |
+ * $$ |  $$ |$$ |$$ | $$ | $$ |\$$$$$$ |$$$$$$$  |\$$$$$$\ \$$$$$$ | \$$$  |$$ |  $$ |
+ * \__|  \__|\__|\__| \__| \__| \_______|\_______/  \_______| \_______|  \____/ \__|  \__|
+ *                                                                                        
+ */
+declare(strict_types=1);
+
+namespace zomarrd\almadeath\permission;
+
+/**
+ * This class is generated automatically, do NOT modify it by hand.
+ */
+final class PermissionKey
+{
+	public const ALMA_COMMAND_CREATE = "alma.command.create";
+	public const ALMA_COMMAND_EDIT = "alma.command.edit";
+	public const ALMA_COMMAND_GIVE = "alma.command.give";
+	public const ALMA_COMMAND_LIST = "alma.command.list";
+	public const ALMA_COMMAND_SETPAY = "alma.command.setpay";
+}

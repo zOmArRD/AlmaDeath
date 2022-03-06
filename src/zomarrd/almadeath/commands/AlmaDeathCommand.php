f9c7e6eeb3c876ade31d9c204689d5718c03aac6
zOmArRD <omarsena25@gmail.com>
2022-03-06 01:11:56 -0400

    Create Command Base

---
+++ src/zomarrd/almadeath/commands/AlmaDeathCommand.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Created by PhpStorm.
+ *
+ * User: zOmArRD
+ * Date: 6/3/2022
+ *
+ * Copyright © 2022 zOmArRD (omar@ghostlymc.live) - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace zomarrd\almadeath\commands;
+
+use CortexPE\Commando\BaseCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\Plugin;
+use zomarrd\almadeath\commands\sub\AlmaCreate;
+use zomarrd\almadeath\commands\sub\AlmaEdit;
+use zomarrd\almadeath\commands\sub\AlmaGive;
+use zomarrd\almadeath\commands\sub\AlmaList;
+use zomarrd\almadeath\commands\sub\AlmaSetPay;
+
+final class AlmaDeathCommand extends BaseCommand
+{
+    public function __construct(Plugin $plugin) { parent::__construct($plugin, 'alma', 'AlmaDeath manager'); }
+
+    protected function prepare(): void
+    {
+        $this->registerSubCommand(new AlmaCreate('create'));
+        $this->registerSubCommand(new AlmaEdit('edit'));
+        $this->registerSubCommand(new AlmaSetPay('setpay'));
+        $this->registerSubCommand(new AlmaGive('give'));
+        $this->registerSubCommand(new AlmaList('list'));
+    }
+
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    {
+        $this->sendUsage();
+    }
+}
---
+++ src/zomarrd/almadeath/commands/sub/AlmaList.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Created by PhpStorm.
+ *
+ * User: zOmArRD
+ * Date: 6/3/2022
+ *
+ * Copyright © 2022 zOmArRD (omar@ghostlymc.live) - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace zomarrd\almadeath\commands\sub;
+
+use CortexPE\Commando\BaseSubCommand;
+use pocketmine\command\CommandSender;
+
+final class AlmaList extends BaseSubCommand
+{
+
+    protected function prepare(): void
+    {
+        // TODO: Implement prepare() method.
+    }
+
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    {
+        // TODO: Implement onRun() method.
+    }
+}
